769c3ffb5d8d7514f69b6bbead90f067d32765e7
nangkhan.rana <nangkhanrana@gmail.com>
2025-11-27 14:51:57 +0545

    get API

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const express= require("express")
+const express = require("express")
 const app = express()
-const {connectDatabase} = require("./database/database.js");
+const { connectDatabase } = require("./database/database.js");
 const Blog = require("./Model/blogModel.js");
 connectDatabase();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //CREATE BLOG API
 app.post("/createBlog", async (req,res)=>{
-    
-    
     //Alternatibe object destructuring  
     const {title,subtitle,description} = req.body
     console.log(req.body);
@@ -28,18 +26,45 @@ app.post("/createBlog", async (req,res)=>{
 
 }),
 
-app.get("/",(req,res)=>{
+//Fetching or reading all data from Blog collection
+// app.get("/getBlog", async (req, res) => {
+//     const blogs = await Blog.find();
+//     if (blogs.length == 0) {
+//         res.json({ 
+//             status: 404,
+//             message: "No blogs found",
+//         });
+//     } else {
+//         res.json({
+//             status: 200,
+//             message: "Blogs fetched successfully",
+//             data: blogs,
+//         });
+//     }
+// });
+//fetching single blog by id
+app.get("/Blog/:id", async (req, res) => {
+    console.log(req.params.id);
+    const id = req.params.id;
+    const blog = await Blog.find({ _id: id });
+    res.status(200).json({
+        message: "Blog fetched successfully",
+        data: blog,
+    });
+}); 
+
+app.get("/", (req, res) => {
     res.send("Hello I am home page");
 });
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.send("This is about page");
 });
 
-app.get("/contact",(req,res)=>{
+app.get("/contact", (req, res) => {
     res.send("This is contact page");
 });
 
-app.listen(5000,(req,res)=>{
+app.listen(5000, (req, res) => {
     console.log("Server is running on port 5000");
 });
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 exports.connectDatabase = async()=>{
-    mongoose.connect(await "mongodb+srv://nangkhanrana:Bttktt.78948@cluster0.0ljrjv6.mongodb.net/?appName=Cluster0");
+    mongoose.connect(await "mongodb+srv://nangkhanrana:Bttktt.78948@cluster0.0ljrjv6.mongodb.net/Blogs?appName=Cluster0");
 
     console.log("Mongodb connected successfully");
 }
